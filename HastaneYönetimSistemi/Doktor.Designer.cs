eaa9e63a0928c1301f0a6eaa4c5fff0e97d670b5
FurkanSayin <76747287+FurkanSayin@users.noreply.github.com>
2023-04-07 23:26:25 +0300

    doktor oluşturuldu

--- HastaneYönetimSistemi/Doktor.Designer.cs
+++ HastaneYönetimSistemi/Doktor.Designer.cs
@@ -35,11 +35,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(270, 101);
+            this.label1.Location = new System.Drawing.Point(353, 188);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(169, 29);
+            this.label1.Size = new System.Drawing.Size(87, 29);
             this.label1.TabIndex = 0;
-            this.label1.Text = "doktor arayüzü";
+            this.label1.Text = "doktor ";
             // 
             // Doktor
             // 
